b2c1ac8512f0ddf3e2ff0c824ac6ef85ae37cd94
JunSubLee <leejs010605@naver.com>
2024-09-27 15:35:06 +0000

    9.28#Styles

--- react-for-beginners/src/routes/Detail.js
+++ react-for-beginners/src/routes/Detail.js
@@ -5,6 +5,8 @@ function Detail() {
   const [loading, setLoading] = useState(true);
   const [movie, setMovie] = useState({});
   const [genres, setGenres] = useState([]);
+
+  useEffect(() => {
   const getMovie = async () => {
     const json = await (
       await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
@@ -14,9 +16,9 @@ function Detail() {
     setGenres(json.data.movie.genres); // TODO setGenres(movie.genres) 로는 세팅이 안됨
     console.log(json);
   };
-  useEffect(() => {
     getMovie();
-  }); // 2번째 인자가 있어서 에러남   React Hook useEffect has a missing dependency: 'getMovie'. Either include it or remove the dependency array
+  },[id]);
+  
   return (
     <div>
       {loading ? (
@@ -27,22 +29,20 @@ function Detail() {
           <h1>{movie.title}</h1>
           <p>
             <span>{movie.year} / </span>
-            {genres.map((g) => (
+            {genres && genres.map((g) => ( //장르가 없는 경우도 있어서 genres && 추가
               <span key={g}>{g} / </span>
             ))}
             <span>{movie.runtime} min</span>
           </p>
-          <p>
-            <b>
+            {/* <b>
               <span style={{ color: "green" }}>
                 Download : {movie.download_count}
               </span>
             </b>
-            &nbsp;
+            &nbsp; */}
             <b>
               <span style={{ color: "red" }}>Like : {movie.like_count}</span>
             </b>
-          </p>
           <p>{movie.description_full}</p>
           <dl>
             <dt>
