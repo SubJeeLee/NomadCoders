a63807ea231ef175e2ebd558ddc06a91cbafb845
JunSubLee <leejs010605@naver.com>
2024-10-02 14:23:41 +0000

    feat: MainCard

--- baemin/components/card/MainCard.tsx
+++ baemin/components/card/MainCard.tsx
@@ -1,3 +1,33 @@
-export default function MainCard({title} : {title: string}){
-    return <section>{title}</section>
+import Image from "next/image";
+
+type PropertyType = {
+  // 필요한 속성 정의
+  title: string;
+  description: string;
+  imageURL: string;
+  // 추가 속성...
+};
+
+export default function MainCard({ property }: { property: PropertyType }) {
+  return (
+    <>
+      <div className="relative w-full p-2 justify-between gap-2">
+        <div className="w-full p-2 bg-white h-32 rounded-xl">
+          <div className="flex flex-col w-full">
+            <h2 className="text-xl font-bold">{property.title}</h2>
+            <p className="w-24">{property.description}</p>
+          </div>
+        </div>
+
+        {/* image */}
+        <Image
+          alt={property.title}
+          src={property.imageURL}
+          width={720}
+          height={720}
+          className="bg-slate-500 rounded-full w-8 h-8 absolute right-4 bottom-4"
+        />
+      </div>
+    </>
+  );
 }
--- baemin/components/section/MainSection.tsx
+++ baemin/components/section/MainSection.tsx
@@ -1,13 +1,41 @@
 import MainCard from "../card/MainCard";
+import DeliveryCard from "../card/DeliveryCard";
+
+type PropertyType = {
+    // 필요한 속성 정의
+    title: string;
+    description: string;
+    imageURL: string;
+    // 추가 속성...
+  };
+
+const property = [
+    {
+        title:"배달",
+        description:  "세상은 넓고 맛집은 없다",
+        imageURL: "/images/1.png",
+    },
+    {
+        title:"B마트",
+        description:  "장보기도 더 빠르게",
+        imageURL: "/images/1.png",
+    },
+    {
+        title:"배민스토어",
+        description:  "배달은 지금 옵니다",
+        imageURL: "/images/1.png",
+    },
+];
 
 export default function MainSection() {
-    return(
-        <section className="h-full">
-            <div className="flex">
-            {["배달","B마트","배민스토어"].map((value: string, index:number)=>(
-            <MainCard key={index} title={value} />   
-            ))}
-            </div>
-        </section>
-    );
+  return (
+    <section className="h-full">
+      <div className="flex">
+        {property.map((value: PropertyType, index: number) => (
+          <MainCard key={index} property={value} />
+        ))}
+      </div>
+        <DeliveryCard />
+    </section>
+  );
 }
